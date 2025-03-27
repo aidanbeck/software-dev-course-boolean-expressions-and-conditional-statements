c7c3392d2a5eb8c8343c76b2acb50f573d2aa2df
aidanbeck <aidanmbeck@outlook.com>
2025-03-27 11:53:34 -0500

    game

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -26,20 +26,41 @@ Paste the following code into your editor:
 
 const readline = require('readline-sync');
 
+const hasCoat = true;
 const hasTorch = true;
-const hasMap = false;
+const hasMap = true;
+const hasKey = true;
+let score = 0;
 
-console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+console.log("You see multiple paths: one leads to the mountains, another to the village, a third to an old house, and a fourth into a cave.");
+const choice = readline.question("Do you go to the 'mountains', 'village', 'house', or 'cave'?");
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
-  console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
+if (choice === "mountains" && hasCoat) {
+  console.log("You safely navigate through the dark mountains, the biting frost kept at bay by your warm coat.");
+  score += 20;
+} else if (choice === "mountains" && !hasCoat) {
+  console.log("The cold wind pierces through your thin clothes until you numb. It doesn't hurt anymore, but you feel very tired. Maybe you'll sit down in the snow, just for a little while...");
+} else if (choice === "village" && hasMap) {
   console.log("You find your way to the village.");
-} else {
+  score += 10;
+} else if (choice === "village" && !hasMap) {
   console.log("You get lost and wander aimlessly.");
+} else if (choice === "house" && hasKey) {
+  console.log("You unlock the front door and enter the house. There is plenty of food here to last a while.");
+  score += 15;
+} else if (choice == "house" && !hasKey) {
+  console.log("The door is locked. You cannot proceed.");
+} else if (choice == "cave" && hasTorch) {
+  console.log("With the torch lighting you way, you delve into the cave. In a dark passage, you find a chest of long lost treasure!");
+  score += 100;
+} else {
+  console.log("Without a torch, you get lost and cannot find your way back.");
+}
+
+if (score > 0) {
+  console.log("You win! Your score is: " + score + ".");
+} else {
+  console.log("You have died. Sorry!");
 }
 
 /* 
